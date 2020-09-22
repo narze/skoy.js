38ae895efb9c97adda786215b5669c6954e1ad8d
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-22 09:37:39 +0700

    feat: convert แ to เเ

--- test/test.js
+++ test/test.js
@@ -1,14 +1,21 @@
+// Mock random
+Math.random = () => 0.0
+
 let expect = require('chai').expect
 let Skoy = require('../lib/skoy')
 
-describe('Skoy', function() {
-  it('can convert to string', function() {
+describe('Skoy', () => {
+  it('converts to string', () => {
     expect(Skoy.convert('สวัสดี')).to.be.a('string')
   })
 
-  it('can convert static words', function() {
-    expect(Skoy.convert('พวก')).to.include('พ๊ก')
+  it('converts static words', () => {
+    expect(Skoy.convert('พวก')).to.include('พ๊ห์ก')
     expect(Skoy.convert('หนู')).to.include('หนุ๊')
+  })
+
+  it('converts แ to double เ', () => {
+    expect(Skoy.convert('แก')).to.include('เเก')
   })
 })
 
